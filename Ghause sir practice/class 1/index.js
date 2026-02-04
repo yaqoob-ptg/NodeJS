3b925df7e77b7cac4bac7e64f7248074f552f4a5
yaqoob-ptg <yaqoob.ptg@gmail.com>
2026-02-04 17:47:53 +0500

    .

--- Ghause sir practice/class 1/index.js
+++ Ghause sir practice/class 1/index.js
@@ -4,22 +4,71 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const port = 3000;
 
 app.get('/',(req,res)=>{
-    res.send('Hello, World!');
+    const date =new Date();
+    const day=date.getDate();
+    const month=date.getMonth()+1;
+    const year=date.getFullYear();
+    const hours=date.getHours();
+    const minutes=date.getMinutes();
+    const seconds=date.getSeconds();
+
+    res.send(`Today's date is ${day}-${month}-${year} and the current time is ${hours}:${minutes}:${seconds}`);
 })
+
+let users=[
+    {id:1,name:'Alice'},
+    {id:2,name:'Bob'},
+    {id:3,name:'Charlie'}
+]
 
 app.get('/users',(req,res)=>{
-    res.send({
-        name: "John Doe",
-        age: 32,
-
-    })
+    res.send(users);
 })
 
-app.post('/data',(req,res)=>{
-    res.send('Data received via POST request');
+
+app.post('/users',(req,res)=>{
+    const {name,id} =req.body;
+    console.log(req.body)
+
+// const date = new Date.now().toString(36);
+    users.push({...req.body,id: Date.now()});
+        res.send({message: `user added successfully ${name} `});
+
+
+})
+
+app.put('users/:id',(req,res)=>{
+    const {id}=req.params;
+    const {name,email}=req.body;
+    const userIndex=users.findIndex(user=>user.id===parseInt(id));
+    if(userIndex===-1){
+        res.status(404).send({message:"User not found"});
+    }else{
+        users[userIndex]={...users[userIndex],name,email};
+        res.send({message:"User updated successfully",user:users[userIndex]});
+    }
+})
+
+app.delete('/users/:id',(req,res)=>{
+    const {id} =req.params;
+    // const userIndex=users.findIndex(user=>user.id===parseInt(id));
+
+
+    // if(userIndex===-1){
+    //     res.status(404).send({message:"User not found"});
+    // }else{
+    //     users.splice(userIndex,1);
+    //     res.send({message:"User deleted successfully"});
+    // }
+
+    users=users.filter(user=>user.id!==parseInt(id));
+    res.send({message:"User deleted successfully",users});
+
 })
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
